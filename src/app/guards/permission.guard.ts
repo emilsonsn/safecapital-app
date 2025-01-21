7cb063cff049196dcc02b02c40bf6ad5ae3b26a8
Gabriel Calebe <105611134+calebetx@users.noreply.github.com>
2025-01-21 20:13:58 -0300

    fix: adiciona guardas nas rotas

--- src/app/guards/permission.guard.ts
+++ src/app/guards/permission.guard.ts
@@ -1,8 +1,32 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, CanMatchFn, Router, ActivatedRouteSnapshot, Route } from '@angular/router';
+import { UserRole } from '@models/user';
 import { SessionQuery } from '@store/session.query';
 import { SessionService } from '@store/session.service';
 import { catchError, map, of } from 'rxjs';
+
+const AdminPermissions = []; // Todas
+
+const ManagerPermissions = [
+  'home',
+  'client',
+  'solicitation',
+  'defaulter',
+  'partners',
+];
+
+const ClientPermissions = [
+  'home',
+  'client',
+  'defaulter',
+  'solicitation',
+];
+
+const permissions = {
+  admin: AdminPermissions,
+  manager: ManagerPermissions,
+  client: ClientPermissions,
+}
 
 export const permissionGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state) => {
   const router = inject(Router);
@@ -10,34 +34,17 @@ export const permissionGuard: CanActivateFn = (route: ActivatedRouteSnapshot, st
   const sessionQuery = inject(SessionQuery);
 
   let page : string = route.data.page;
-  let hasPermission = true;
 
   return sessionQuery.user$.pipe(
     map(user => {
-
-      // if(!user) {
-      //   router.navigate(['/login']);
-      //   hasPermission = false;
-      // }
-      // else {
-
-      //   const notAllowedPermissionToRequester = [
-      //     'home',
-      //     'collaborator',
-      //     'construction',
-      //     'provider',
-      //     'services',
-      //     'tasks',
-      //     'client'
-      //   ];
-
-      //   if (user?.company_position?.position == 'Requester' && notAllowedPermissionToRequester.includes(page))
-      //     hasPermission = false;
-
-      // }
-      return true;
-
-      return hasPermission;
+      if(!user) {
+        router.navigate(['/login']);
+      }
+      else {
+        if(user?.role.toLowerCase() == UserRole.Admin.toLowerCase()) return true;
+        else if (permissions[user?.role.toLowerCase()].includes(page)) return true;
+      }
+      return false;
     }),
     catchError(error => {
       console.error('Permission check error:', error);
