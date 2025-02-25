05208d33c03dd63d4bf733f400e5bb4799c69466
Gabriel Calebe <105611134+calebetx@users.noreply.github.com>
2025-02-24 21:41:55 -0300

    fix: tipo de arquivo no file-unique

--- src/app/shared/components/file-unique-upload/file-unique-upload.component.ts
+++ src/app/shared/components/file-unique-upload/file-unique-upload.component.ts
@@ -1,5 +1,12 @@
-import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { RequiredFilesEnum } from '@app/views/session/register/register/register.component';
+import { ToastrService } from 'ngx-toastr';
 
 export interface FileUniqueProps {
   id: number;
@@ -15,11 +22,10 @@ export interface FileUniqueProps {
   styleUrl: './file-unique-upload.component.scss',
 })
 export class FileUniqueUploadComponent {
-
   // Utils
-  @Input() public containsImage : boolean = true;
+  @Input() public containsImage: boolean = true;
 
-  @Input() public disabled : boolean = false;
+  @Input() public disabled: boolean = false;
 
   // Info
   @Input() public category: RequiredFilesEnum | string;
@@ -35,19 +41,29 @@ export class FileUniqueUploadComponent {
   protected isDragOver: boolean = false;
 
   // Emitters
-  @Output() onFileChange: EventEmitter<FileUniqueProps> = new EventEmitter<FileUniqueProps>();
-  @Output() onFileDelete: EventEmitter<FileUniqueProps> = new EventEmitter<FileUniqueProps>();
+  @Output() onFileChange: EventEmitter<FileUniqueProps> =
+    new EventEmitter<FileUniqueProps>();
+  @Output() onFileDelete: EventEmitter<FileUniqueProps> =
+    new EventEmitter<FileUniqueProps>();
 
   // Utils
-  private defaultValues : FileUniqueProps = {
+  private defaultValues: FileUniqueProps = {
     id: 0,
     category: null,
     file: null,
     file_name: '',
     preview: null,
-  }
+  };
 
-  constructor() {}
+  public allowedTypes = [
+    'image/png',
+    'image/jpeg',
+    'image/jpg',
+    'application/pdf',
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  ];
+
+  constructor(private readonly _toastr: ToastrService) {}
   ngOnInit() {
     if (this.category) {
       this.defaultValues.category = this.category;
@@ -55,33 +71,44 @@ export class FileUniqueUploadComponent {
   }
 
   protected onFileSelect(e: Event): void {
-    if(this.disabled) return;
+    if (this.disabled) return;
 
     const file = (e.target as HTMLInputElement).files?.[0];
+
+    if (!this.allowedTypes.includes(file.type)) {
+      this._toastr.error(`${file.type} não é permitido`);
+      return;
+    }
+
     if (file) this.insertFile(file);
   }
 
   // Drag
   protected onDragOver(e: DragEvent): void {
-    if(this.disabled) return;
+    if (this.disabled) return;
 
     e.preventDefault();
     this.isDragOver = true;
   }
 
   protected onDragLeave(e: DragEvent): void {
-    if(this.disabled) return;
+    if (this.disabled) return;
 
     this.isDragOver = false;
   }
 
   protected onDrop(e: DragEvent): void {
-    if(this.disabled) return;
+    if (this.disabled) return;
 
     e.preventDefault();
     this.isDragOver = false;
 
     const file = e.dataTransfer?.files[0];
+
+    if (!this.allowedTypes.includes(file.type)) {
+      this._toastr.error(`${file.type} não é permitido`);
+      return;
+    }
 
     this.clearFileUnique();
 
@@ -90,14 +117,16 @@ export class FileUniqueUploadComponent {
 
   // Utils
   protected triggerFileInput(): void {
-    if(this.disabled) return;
+    if (this.disabled) return;
 
-    const fileInput = document.getElementById(this.category + '-input') as HTMLInputElement;
+    const fileInput = document.getElementById(
+      this.category + '-input'
+    ) as HTMLInputElement;
     fileInput.click();
   }
 
   protected insertFile(file: File) {
-    if(this.disabled) return;
+    if (this.disabled) return;
 
     this.fileUnique.file = file;
     this.fileUnique.file_name = file.name;
@@ -111,21 +140,22 @@ export class FileUniqueUploadComponent {
     this.onFileChange.emit(this.fileUnique);
   }
 
-  protected open(e : Event) {
-    if(this.disabled) return;
+  protected open(e: Event) {
+    if (this.disabled) return;
 
     e.stopPropagation();
 
     let fileUrl;
 
-    if(!this.fileUnique.preview.toString().startsWith('data:')) fileUrl = this.fileUnique.preview;
+    if (!this.fileUnique.preview.toString().startsWith('data:'))
+      fileUrl = this.fileUnique.preview;
     else fileUrl = URL.createObjectURL(this.fileUnique.file);
 
     window.open(fileUrl, '_blank');
   }
 
   protected remove(e?: Event): void {
-    if(this.disabled) return;
+    if (this.disabled) return;
 
     e?.stopPropagation();
 
@@ -135,9 +165,11 @@ export class FileUniqueUploadComponent {
   }
 
   protected clearFileUnique() {
-    if(this.disabled) return;
+    if (this.disabled) return;
 
-    const fileInput = document.getElementById(this.category + '-input') as HTMLInputElement;
+    const fileInput = document.getElementById(
+      this.category + '-input'
+    ) as HTMLInputElement;
 
     fileInput.value = '';
     this.fileUnique = { ...this.defaultValues };
